4a8acdcc51bb9441f5432f6a215ffe739fe680c3
Jeff Standen <jeff@webgroupmedia.com>
2012-01-07 14:49:27 -0800

    Added Virtual Attendant behavior + macro variable support

--- api/events/domain_macro.php
+++ api/events/domain_macro.php
@@ -6,13 +6,14 @@ class Event_DomainMacro extends AbstractEvent_Domain {
 		$this->_event_id = self::ID;
 	}
 	
-	static function trigger($trigger_id, $domain_id) {
+	static function trigger($trigger_id, $domain_id, $variables=array()) {
 		$events = DevblocksPlatform::getEventService();
 		$events->trigger(
 	        new Model_DevblocksEvent(
 	            self::ID,
                 array(
                     'domain_id' => $domain_id,
+                    '_variables' => $variables,
                 	'_whisper' => array(
                 		'_trigger_id' => array($trigger_id),
                 	),
