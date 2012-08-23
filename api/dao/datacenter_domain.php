4ce93acfe6b6e3dd9822146a867036501b992e35
Jeff Standen <jeff@webgroupmedia.com>
2012-08-22 18:54:13 -0700

    6.1 compatibility

--- api/dao/datacenter_domain.php
+++ api/dao/datacenter_domain.php
@@ -490,20 +490,16 @@ class DAO_Domain extends C4_ORMHelper {
 		// Translate virtual fields
 		
 		$args = array(
-			'join_sql' => $join_sql,
-			'where_sql' => $where_sql,
-			'has_multiple_values' => $has_multiple_values
+			'join_sql' => &$join_sql,
+			'where_sql' => &$where_sql,
+			'has_multiple_values' => &$has_multiple_values
 		);
 		
 		array_walk_recursive(
 			$params,
 			array('DAO_Domain', '_translateVirtualParameters'),
-			&$args
+			$args
 		);
-		
-		$join_sql = $args['join_sql'];
-		$where_sql = $args['where_sql'];
-		$has_multiple_values = $args['has_multiple_values'];
 		
 		$result = array(
 			'primary_table' => 'datacenter_domain',
