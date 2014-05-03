2d9c7245cedc67f8d3a6f38e2a479e34a8316aff
Jeff Standen <jeff@webgroupmedia.com>
2014-05-03 16:27:14 -0700

    6.7.2 compatibility

--- api/rest/domains.php
+++ api/rest/domains.php
@@ -78,6 +78,21 @@ class ChRest_Domains extends Extension_RestController implements IExtensionRestC
 				'name' => DAO_Domain::NAME,
 				'server_id' => DAO_Domain::SERVER_ID,
 			);
+			
+		} elseif ('subtotal'==$type) {
+			$tokens = array(
+				'fieldsets' => SearchFields_Domain::VIRTUAL_HAS_FIELDSET,
+				'links' => SearchFields_Domain::VIRTUAL_CONTEXT_LINK,
+				'watchers' => SearchFields_Domain::VIRTUAL_WATCHERS,
+					
+				'server' => SearchFields_Domain::SERVER_ID,
+			);
+			
+			$tokens_cfields = $this->_handleSearchTokensCustomFields(CerberusContexts::CONTEXT_DOMAIN);
+			
+			if(is_array($tokens_cfields))
+				$tokens = array_merge($tokens, $tokens_cfields);
+			
 		} else {
 			$tokens = array(
 				'created' => SearchFields_Domain::CREATED,
@@ -93,10 +108,10 @@ class ChRest_Domains extends Extension_RestController implements IExtensionRestC
 		return NULL;
 	}
 
-	function getContext($id) {
+	function getContext($model) {
 		$labels = array();
 		$values = array();
-		$context = CerberusContexts::getContext(CerberusContexts::CONTEXT_DOMAIN, $id, $labels, $values, null, true);
+		$context = CerberusContexts::getContext(CerberusContexts::CONTEXT_DOMAIN, $model, $labels, $values, null, true);
 
 		return $values;
 	}
@@ -115,7 +130,10 @@ class ChRest_Domains extends Extension_RestController implements IExtensionRestC
 		$this->error(self::ERRNO_CUSTOM, sprintf("Invalid domain id '%d'", $id));
 	}
 	
-	function search($filters=array(), $sortToken='id', $sortAsc=1, $page=1, $limit=10) {
+	function search($filters=array(), $sortToken='id', $sortAsc=1, $page=1, $limit=10, $options=array()) {
+		@$show_results = DevblocksPlatform::importVar($options['show_results'], 'boolean', true);
+		@$subtotals = DevblocksPlatform::importVar($options['subtotals'], 'array', array());
+		
 		$worker = CerberusApplication::getActiveWorker();
 
 		$custom_field_params = $this->_handleSearchBuildParamsCustomFields($filters, CerberusContexts::CONTEXT_DOMAIN);
@@ -127,29 +145,48 @@ class ChRest_Domains extends Extension_RestController implements IExtensionRestC
 		$sortAsc = !empty($sortAsc) ? true : false;
 		
 		// Search
-		list($results, $total) = DAO_Domain::search(
-			!empty($sortBy) ? array($sortBy) : array(),
+		
+		$view = $this->_getSearchView(
+			CerberusContexts::CONTEXT_DOMAIN,
 			$params,
 			$limit,
-			max(0,$page-1),
+			$page,
 			$sortBy,
-			$sortAsc,
-			true
+			$sortAsc
 		);
 		
-		$objects = array();
+		if($show_results)
+			list($results, $total) = $view->getData();
 		
-		foreach($results as $id => $result) {
-			$values = $this->getContext($id);
-			$objects[$id] = $values;
+		// Get subtotal data, if provided
+		if(!empty($subtotals))
+			$subtotal_data = $this->_handleSearchSubtotals($view, $subtotals);
+		
+		if($show_results) {
+			$objects = array();
+			
+			$models = CerberusContexts::getModels(CerberusContexts::CONTEXT_DOMAIN, array_keys($results));
+			
+			unset($results);
+			
+			foreach($models as $id => $model) {
+				$values = $this->getContext($model);
+				$objects[$id] = $values;
+			}
 		}
 		
-		$container = array(
-			'total' => $total,
-			'count' => count($objects),
-			'page' => $page,
-			'results' => $objects,
-		);
+		$container = array();
+		
+		if($show_results) {
+			$container['results'] = $objects;
+			$container['total'] = $total;
+			$container['count'] = count($objects);
+			$container['page'] = $page;
+		}
+		
+		if(!empty($subtotals)) {
+			$container['subtotals'] = $subtotal_data;
+		}
 		
 		return $container;
 	}
