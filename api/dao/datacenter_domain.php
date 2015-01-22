859c454906a02f0f40e2ad565b43a20d0fbbe163
Jeff Standen <jeff@webgroupmedia.com>
2015-01-21 18:17:33 -0800

    6.9 API compatibility

--- api/dao/datacenter_domain.php
+++ api/dao/datacenter_domain.php
@@ -1015,6 +1015,19 @@ class View_Domain extends C4_AbstractView implements IAbstractView_Subtotals, IA
 		$fields = self::_appendFieldsFromQuickSearchContext(CerberusContexts::CONTEXT_DOMAIN, $fields, null);
 		$fields = self::_appendFieldsFromQuickSearchContext(CerberusContexts::CONTEXT_SERVER, $fields, 'server');
 		
+		// Engine/schema examples: Comments
+		
+		$ft_examples = array();
+		
+		if(false != ($schema = Extension_DevblocksSearchSchema::get(Search_CommentContent::ID))) {
+			if(false != ($engine = $schema->getEngine())) {
+				$ft_examples = $engine->getQuickSearchExamples($schema);
+			}
+		}
+		
+		if(!empty($ft_examples))
+			$fields['comments']['examples'] = $ft_examples;
+		
 		// Sort by keys
 		
 		ksort($fields);
