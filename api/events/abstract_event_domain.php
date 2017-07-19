e9c5bde2b83711c1d656313ecae205a5ba510078
Jeff Standen <jeff@webgroupmedia.com>
2017-07-19 01:32:57 -0700

    8.0 compatibility

--- api/events/abstract_event_domain.php
+++ api/events/abstract_event_domain.php
@@ -100,7 +100,7 @@ abstract class AbstractEvent_Domain extends Extension_DevblocksEvent {
 				'context' => CerberusContexts::CONTEXT_BEHAVIOR,
 			),
 			'behavior_bot_id' => array(
-				'label' => 'Behavior',
+				'label' => 'Bot',
 				'context' => CerberusContexts::CONTEXT_BOT,
 			),
 			'domain_id' => array(
